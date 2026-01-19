0ac21dd3be91effdccb8afc94bfbc14cd81d5ae9
Sirapop Chanphui <srp073536@gmail.com>
2026-01-19 19:13:42 +0700

    feat: update Navbar and DesktopMenu for navigation integration

--- src/components/Navbar/DesktopMenu.jsx
+++ src/components/Navbar/DesktopMenu.jsx
@@ -1,10 +1,12 @@
 import Button from "../common/Button";
+import { useNavigate } from "react-router-dom";
 
 function DesktopMenu() {
+  const navigate = useNavigate();
   return (
     <div className="hidden 2xl:flex flex-row gap-[8px]">
-      <Button buttonText="Log in" buttonStyle="secondary" />
-      <Button buttonText="Sign up" buttonStyle="primary" />
+      <Button buttonText="Log in" buttonStyle="secondary" onClick={() => navigate("/login")} />
+      <Button buttonText="Sign up" buttonStyle="primary" onClick={() => navigate("/signup")} />
     </div>
   );
 }
