db4dcbbaa2c0e6b41a1c2236ea0125690e09c218
Sirapop Chanphui <srp073536@gmail.com>
2026-01-23 00:05:46 +0700

    feat: add profile and reset password pages with user/admin layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+
+import UserLayout from "./layouts/UserLayout";
+import AdminLayout from "./layouts/AdminLayout";
+
 import HomePage from "./pages/HomePage";
 import ArticleDetailPage from "./pages/ArticleDetailPage";
-import { Toaster } from "sonner";
-import Navbar from "./components/Navbar/Navbar";
 import SignUpPage from "./pages/SignUpPage";
 import RegistrationSuccess from "./pages/RegistrationSuccessPage";
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
+import ProfilePage from "./pages/ProfilePage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import AdminArticleManagement from "./pages/Admin/AdminArticleManagement";
 
 function App() {
   return (
     <BrowserRouter>
-    <Toaster position="bottom-right" richColors />
-    <Navbar/>
+      <Toaster position="bottom-right" richColors />
+
       <Routes>
-      <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signup/success" element={<RegistrationSuccess />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/article/:id" element={<ArticleDetailPage />} />
+        <Route path="*" element={<NotFound />} />
+
+        {/* USER LAYOUT */}
+        <Route element={<UserLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signup/success" element={<RegistrationSuccess />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login/profile" element={<ProfilePage />} />
+          <Route path="/login/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/article/:id" element={<ArticleDetailPage />} />
+        </Route>
+
+        {/* ADMIN LAYOUT */}
+        <Route path="/login/admin" element={<AdminLayout />}>
+          <Route path="article-management"element={<AdminArticleManagement />}/>
+        </Route>
 
       </Routes>
     </BrowserRouter>
---
+++ src/layouts/AdminLayout.jsx
@@ -0,0 +1,16 @@
+import AdminSidebar from "../components/Admin/AdminSidebar"; 
+import { Outlet } from "react-router-dom";
+
+function AdminLayout() {
+    return (
+        <div className="flex">
+
+            <AdminSidebar />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default AdminLayout;
